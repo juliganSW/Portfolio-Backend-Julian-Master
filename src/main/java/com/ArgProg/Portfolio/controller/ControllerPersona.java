231ecddf5d9cf647b0101d71ad5231ec6b113d21
unknown <juligan@gmail.com>
2023-02-21 23:39:52 -0300

    nuevos cambios

--- src/main/java/com/ArgProg/Portfolio/controller/ControllerPersona.java
+++ src/main/java/com/ArgProg/Portfolio/controller/ControllerPersona.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("persona")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-backend-xube.onrender.com")
 @Table(name = "persona")
 
 public class ControllerPersona {
